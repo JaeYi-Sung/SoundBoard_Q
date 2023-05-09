c9b53cb911aeece63381ccb186262fdc07e86468
Jae Yi Sung <jaeyisung@JaeYi-MacBook-Air.local>
2023-05-09 14:24:02 +0900

    develop ver

--- SoundBoard_Q/ViewController.swift
+++ SoundBoard_Q/ViewController.swift
@@ -7,14 +7,62 @@
 
 import UIKit
 import AVFoundation
+import MediaPlayer
 
 class ViewController: UIViewController {
     
+    private var springImg: UIImageView!
+    //private var Btn: UIButton!
+    //private let summerImg: UIImageView! = nil
+    //private let autumnImg: UIImageView! = nil
+    //private let let winterImg: UIImageView!
+    
     var audioPlayer : AVAudioPlayer?  //optional로 선언
+    var isPlay : Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        audiosessionInit()
+    }
+    
+    func backImg() {
+        
+        springImg = UIImageView(frame: CGRect(x: 0.0, y: 0.0, width: view.frame.width * 0.5, height: view.frame.height*0.5 ))
+        springImg.image = UIImage(named: "Q - 민들레.png")
+        springImg.contentMode = .scaleAspectFill
+        view.addSubview(springImg)
+        
+//        springBtn = UIButton(frame: CGRect(x: Int(0.5*springImg.frame.maxX), y: Int(0.5*springImg.frame.maxY), width: springImg.frame.width*0.8, height: springImg.frame.height * 0.2))
+//        springBtn.backgroundColor = .blue
+        
+    }
+    
+    func audiosessionInit() {
+        //Audio Session 설정
+        let audiosession = AVAudioSession.sharedInstance()
+        do {
+            try audiosession.setCategory(.playback, mode: .default, options: [])
+            
+        } catch let error as NSError {
+            print("audioSession 오류 : \(error.localizedDescription)")
+        }
+    }
+    
+    
+    @IBAction func pauseAction(_ sender: Any) {
+        
+        if isPlay == 1 {
+            audioPlayer?.pause()
+            isPlay = 0
+        }
+
+        else {
+            audioPlayer?.play()
+            isPlay = 1
+        }
+        
+        //audioPlayer?.pause()
     }
     
     
@@ -32,6 +80,7 @@ class ViewController: UIViewController {
         do {
             audioPlayer = try AVAudioPlayer(contentsOf: url!)
             audioPlayer?.play()
+            isPlay = 1
         }
         catch {
             print("error")
@@ -52,6 +101,7 @@ class ViewController: UIViewController {
         do {
             audioPlayer = try AVAudioPlayer(contentsOf: url!)
             audioPlayer?.play()
+            isPlay = 1
         }
         catch {
             print("error")
@@ -72,6 +122,7 @@ class ViewController: UIViewController {
         do {
             audioPlayer = try AVAudioPlayer(contentsOf: url!)
             audioPlayer?.play()
+            isPlay = 1
         }
         catch {
             print("error")
@@ -92,6 +143,7 @@ class ViewController: UIViewController {
         do {
             audioPlayer = try AVAudioPlayer(contentsOf: url!)
             audioPlayer?.play()
+            isPlay = 1
         }
         catch {
             print("error")
